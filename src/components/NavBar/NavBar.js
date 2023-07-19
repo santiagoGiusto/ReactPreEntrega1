46ad8dfd4f207847115d493b9f818d1de9daa7fb
santiagoGiusto <giustosantiago1@gmail.com>
2023-07-19 12:07:09 -0300

    reactPreEntrega1SantiagoGiusto

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -11,9 +11,9 @@ const NavBar = () => {
         <Nav className="flex-column my-4">
             <h3>CoderHouse react - Pre entrega 1 - Santiago Giusto</h3>
             <Navbar className="d-flex justify-content-around my-4" bg="warning" data-bs-theme="dark">
-                <Button variant="outline-dark" >BOTON 1</Button>
-                <Button variant="outline-dark">BOTON 2</Button>
-                <Button variant="outline-dark">BOTON 3</Button>
+                <Button variant="dark" >BOTON 1</Button>
+                <Button variant="dark">BOTON 2</Button>
+                <Button variant="dark">BOTON 3</Button>
              </Navbar>
          <CartWidget/>
         </Nav>
